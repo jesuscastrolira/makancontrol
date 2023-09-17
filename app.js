188ad70db1d2a99f242a3845751f0992e331bf80
jesuscastrolira <jesusgregoriocastrolira@gmail.com>
2023-09-17 23:48:19 +0200

    Añadido nuevo estilo CSS para hacer la aplicación responsive. Añadí tambien los estilos minimalistas a la pagina principal. Diseño de la primera página.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Puerto en el que se ejecutará el servidor
+
+// Configuración de rutas estáticas para los archivos HTML, CSS y JavaScript
+app.use(express.static(__dirname));
+
+// Ruta principal que sirve la página HTML
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // Reemplaza 'index.html' con el nombre de tu archivo HTML principal
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
